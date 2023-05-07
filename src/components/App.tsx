cba133fa613a957c656a72b6f270b96c2bd89dd3
ilya-galyas <galasila2@gmail.com>
2023-05-07 13:38:14 +0300

    1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,9 @@
-export const App = () => {
-    return (<div>
 
+export const App = () => {
+    return (<div className='bg-red-500'>
+        <h1 className="text-3xl font-bold underline fill-amber-200">
+            Hello world!
+        </h1>
+        <p className={css.paragraph} > fdgdgdgdgdfgdg</p>
     </div>)
 }
