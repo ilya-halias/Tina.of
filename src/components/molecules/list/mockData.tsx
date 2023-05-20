1a2cc914b29b63882a28b6539438c40bfb6fc842
ilya-galyas <galasila2@gmail.com>
2023-05-20 18:27:41 +0300

    create descriptions

---
+++ src/components/molecules/list/mockData.tsx
@@ -0,0 +1,25 @@
+//data
+import {dataText} from "../../../translate/ru/dataRu";
+// types
+import {DescribeListType} from "./mockDataType";
+import {EquipmentListType} from "./mockDataType"
+
+export const descriptionList:DescribeListType = [
+    {id: 1, title: dataText.describes["1"]},
+    {id: 2, title: dataText.describes["2"]},
+    {id: 3, title: dataText.describes["3"]},
+    {id: 4, title: dataText.describes["4"]},
+    {id: 5, title: dataText.describes["5"]},
+    {id: 6, title: dataText.describes["6"]},
+    {id: 7, title: dataText.describes["7"]},
+    {id: 8, title: dataText.describes["8"]},
+    {id: 9, title: dataText.describes["9"]},
+
+]
+
+export const equipmentList:EquipmentListType = [
+    {id:1, title: dataText.equipment["1"]},
+    {id:2, title: dataText.equipment["2"]},
+    {id:3, title: dataText.equipment["3"]},
+    {id:4, title: dataText.equipment["4"]},
+]
