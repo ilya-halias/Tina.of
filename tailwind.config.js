4fb1154bf6cc8ae0e218f7ecc1c33f4fa2c36743
ilya-galyas <galasila2@gmail.com>
2023-05-20 18:58:29 +0300

    create descriptions

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx,css}", "./public/index.html"],
+  theme: {
+    colors: {
+      dark: {
+        100: "#232323",
+      },
+      white:{
+        100: "#FFFFFF"
+      }
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
